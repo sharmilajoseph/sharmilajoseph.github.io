3e6ca5d0a8b180394b6794b03a3816fcfbfc7b6c
sharmilajoseph <95367974+sharmilajoseph@users.noreply.github.com>
2022-01-08 22:14:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 ( function (window){  
 var string = ["Yaakov", "John", "jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura","Jygvt" , "Jim"];
-
      
     for (var i = 0; i < string.length; i++) {
-
     var firstletter =string[i].toLowerCase();
 
      if ( string[i].charAt(0)=="J" || firstletter.charAt(0)=="j") {
     	byeSpeaker.speak(string[i]);
-
-    } else {
-   	helloSpeaker.speak (string[i]);
+  } else {
+   	helloSpeaker.speak(string[i]);
     };
-
-    	}
-
-    	window.string=string;
+}
+     window.string=string;
 })(window);
 
    
